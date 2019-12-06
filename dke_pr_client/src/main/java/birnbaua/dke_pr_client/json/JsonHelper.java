8d3db2764a1706feb8ca75d68727203c8191ac94
Birnbauaa <andreas.bachl@24speed.at>
2019-12-06 19:56:49 +0100

    json helper init

--- dke_pr_client/src/main/java/birnbaua/dke_pr_client/json/JsonHelper.java
+++ dke_pr_client/src/main/java/birnbaua/dke_pr_client/json/JsonHelper.java
@@ -1,5 +1,21 @@
 package birnbaua.dke_pr_client.json;
 
-public class JsonHelper {
+import java.lang.reflect.Type;
+import java.util.List;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+public class JsonHelper {
+	
+	public static <T> String getJsonFrom(List<T> objects, Class<T> c) {
+		
+		//TODO
+		return null;
+	}
+	
+	public static <T> List<T> getObjectsFrom(String json, Class<T> c) {
+		Type listType = new TypeToken<List<T>>() {}.getType();
+		return new Gson().fromJson(json,listType);
+	}
 }
--- dke_pr_client/src/main/java/module-info.java
+++ dke_pr_client/src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module birnbaua.dke_pr_client {
     requires javafx.controls;
     requires javafx.fxml;
+	requires gson;
 
     opens birnbaua.dke_pr_client to javafx.fxml;
     exports birnbaua.dke_pr_client;
