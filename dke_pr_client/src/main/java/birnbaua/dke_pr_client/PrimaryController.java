22409ef45cdcae4a7f88691515d943bcd0f89e14
Birnbauaa <andreas.bachl@24speed.at>
2019-12-09 18:34:46 +0100

    test row, added properties

--- dke_pr_client/src/main/java/birnbaua/dke_pr_client/PrimaryController.java
+++ dke_pr_client/src/main/java/birnbaua/dke_pr_client/PrimaryController.java
@@ -1,14 +1,20 @@
 package birnbaua.dke_pr_client;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.Properties;
 
 import birnbaua.dke_pr_client.basics.Course;
 import birnbaua.dke_pr_client.basics.University;
+import birnbaua.dke_pr_client.rest.ConnectionHelper;
 import javafx.fxml.FXML;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.CheckBoxTableCell;
 import javafx.scene.text.Text;
 
 public class PrimaryController {
@@ -19,24 +25,45 @@ public class PrimaryController {
     @FXML private Text name;
     @FXML private Text matrNr;
     @FXML private Text university;
+    private Properties properties = new Properties();
 
     @FXML
     void initialize() {
+    	try {
+			properties.load(new FileInputStream(new File(App.class.getResource("settings.properties").getPath()).getAbsolutePath()));
+		} catch (IOException e) {e.printStackTrace();} 
     	courseTableSetup(myCourses);
+    	myCourses.getItems().add(new Course("weihnachtsmensa","6969","max muehler","drangln",6,false));
     }
     
-    private void courseTableSetup(TableView<Course> table) {
+    @SuppressWarnings("unchecked")
+	private void courseTableSetup(TableView<Course> table) {
     	TableColumn<Course,String> title = new TableColumn<>("Title");
     	TableColumn<Course,String> id = new TableColumn<>("Course ID");
     	TableColumn<Course,String> type = new TableColumn<>("Type");
-    	TableColumn<Course,String> ects = new TableColumn<>("Ects");
+    	TableColumn<Course,Number> ects = new TableColumn<>("Ects");
     	TableColumn<Course,String> lector = new TableColumn<>("Lector");
     	TableColumn<Course,Boolean> isEnrolledBy = new TableColumn<>("Enrolled");
     	
+    	table.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
     	table.setEditable(true);
     	isEnrolledBy.setEditable(true);
+    	title.setPrefWidth(50);
+    	id.setPrefWidth(50);
+    	type.setPrefWidth(50);
+    	ects.setPrefWidth(50);
+    	lector.setPrefWidth(50);
+    	isEnrolledBy.setPrefWidth(50);
     	
+    	title.setCellValueFactory(value -> value.getValue().getTitle());
+    	id.setCellValueFactory(value -> value.getValue().getId());
+    	type.setCellValueFactory(value -> value.getValue().getType());
+    	ects.setCellValueFactory(value -> value.getValue().getEcts());
+    	lector.setCellValueFactory(value -> value.getValue().getLector());
+    	isEnrolledBy.setCellValueFactory(value -> value.getValue().getIsEnrolledBy());
     	
+    	isEnrolledBy.setCellFactory(CheckBoxTableCell.forTableColumn(isEnrolledBy));
     	
+    	table.getColumns().addAll(title,id,type,ects,lector,isEnrolledBy);
     }
 }
