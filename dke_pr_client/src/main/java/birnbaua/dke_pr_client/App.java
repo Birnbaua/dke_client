6402456109201996e6cf8e7fc8d2263aea214fc2
Birnbauaa <andreas.bachl@24speed.at>
2019-12-09 17:18:38 +0100

    project cleanup

--- dke_pr_client/src/main/java/birnbaua/dke_pr_client/App.java
+++ dke_pr_client/src/main/java/birnbaua/dke_pr_client/App.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -13,26 +14,19 @@ import java.io.IOException;
  */
 public class App extends Application {
 
-    private static Scene scene;
-
-    @Override
-    public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"), 640, 480);
-        stage.setScene(scene);
-        stage.show();
-    }
-
-    static void setRoot(String fxml) throws IOException {
-        scene.setRoot(loadFXML(fxml));
-    }
-
-    private static Parent loadFXML(String fxml) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
-        return fxmlLoader.load();
-    }
-
-    public static void main(String[] args) {
-        launch();
-    }
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		launch(); //launch javafx application
+	}
+	
+	@Override
+	public void start(Stage stage) throws Exception {
+		// TODO Auto-generated method stub
+		BorderPane mainBorderPane = (BorderPane) FXMLLoader.load(App.class.getResource("primary.fxml"));
+		Scene scene = new Scene(mainBorderPane);
+		//scene.getStylesheets().add("main/gui.css");
+		stage.setScene(scene);
+		stage.show();
+	}
 
 }
--- dke_pr_client/src/main/java/module-info.java
+++ dke_pr_client/src/main/java/module-info.java
@@ -2,7 +2,6 @@ module birnbaua.dke_pr_client {
     requires javafx.controls;
     requires javafx.fxml;
 	requires gson;
-
     opens birnbaua.dke_pr_client to javafx.fxml;
     exports birnbaua.dke_pr_client;
 }
